d4185f4ded25a625128d4fb34342277ae9b94221
leriche03.lemuel <lemuel@Elis-Macbook-Pro.local>
2020-11-23 18:29:54 -0500

    adding WeatherCard.jsx

--- client/src/components/WeatherCard.jsx
+++ client/src/components/WeatherCard.jsx
@@ -1,3 +1,26 @@
+import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+
+function WeatherCard({ apiData }) {
+  const [feels_like, setFeelsLike] = useState('');
+  const [mainTemp, setMainTemp] = useState('');
+  const [description, setDescription] = useState('');
+  const [main, setMain] = useState('');
+  const [iconID, setIconID] = useState('');
+
+  return (
+    <div>
+      {apiData?.data?.main?.feels_like}
+      {/* <h1>Main Temperature : {mainTemp} Degrees Celsius</h1>
+      <h1>Feels like: {feels_like} Degrees Celsius</h1>
+      {/* <h1>Weather Parameter: {main}</h1> */}
+      {/* <h1>Description : {description}</h1> */}
+      {/* <img src={'http://openweathermap.org/img/wn/' + iconID + '@2x.png'} /> */}{' '}
+    </div>
+  );
+}
+export default WeatherCard;
+
 // import React from 'react';
 // import { Card } from 'react-bootstrap';
 
